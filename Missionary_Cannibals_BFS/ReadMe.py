9a80fdbf37cdc5e31d60f1bf2ae7d6313979691c
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-06-04 19:44:21 -0400

    ReadMe file

---
+++ Missionary_Cannibals_BFS/ReadMe.py
@@ -0,0 +1,19 @@
+# Implementation of the classical Missionaries and Cannibals problem using Breadth First Search
+
+Code: missionaries_cannibals_BFS.py
+
+Running_command: $python missionaries_cannibals_BFiS.py 
+
+Sample Output:
+Cannibals and Missionary problem: Breadth First search implementation
+Left Status=(3,3,1), Left to right Boat Contains=(1,1,1)
+Left Status=(2,2,0), Right to Left Boat Contains=(1,0,1)
+Left Status=(3,2,1), Left to right Boat Contains=(1,1,1)
+Left Status=(2,1,0), Right to Left Boat Contains=(0,1,1)
+Left Status=(2,2,1), Left to right Boat Contains=(1,1,1)
+Left Status=(1,1,0), Right to Left Boat Contains=(1,0,1)
+Left Status=(2,1,1), Left to right Boat Contains=(1,1,1)
+Left Status=(1,0,0), Right to Left Boat Contains=(0,1,1)
+Left Status=(1,1,1), Left to right Boat Contains=(1,1,1)
+Left Status=(0,0,0), Right to Left Boat Contains=(0,0,0)
+
