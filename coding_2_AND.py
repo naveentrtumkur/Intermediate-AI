c815a9a6885a370f076f12a1984613ccfd6f07ab
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-06-27 17:54:20 -0400

    Ai implement AND learning by using perceptron
    perceptron

---
+++ coding_2_AND.py
@@ -0,0 +1,44 @@
+# Coding Question_2
+# Implementation of perceptron that can learn the boolean
+# function AND using threshold activation function (Can use sigmoid or tanh in our implemenetation). 
+
+import numpy as np
+
+
+# Function definition to perform sigmoid.
+def applySigmoid(x, giveMeTheDerivative = False):
+	if(giveMeTheDerivative == True):
+		return applySigmoid(x) * (1 - applySigmoid(x))
+	return 1 / (1 + np.exp(-x))
+
+def print_data(iter, inputs, keys, weights, prediction):
+	print "This is iteration # ", iter
+	print "The original input data was... \n", inputs
+	print "Your weights at this specific iteration are... \n", weights
+	print "Our prediction at this iteration was... \n", prediction
+
+def train(inputs, keys, weights):
+	for iter in xrange(20000):
+		prediction = applySigmoid(np.dot(inputs, weights))
+		error = keys - prediction
+		change_in_error = error * applySigmoid(prediction,True)
+		weights += np.dot(inputs.T ,change_in_error)
+		if(iter == 0 or iter == 9999):
+			print_data(iter, inputs, keys, weights, prediction)
+
+	print "Output After Training:"
+	print prediction
+
+def main():
+	np.random.seed(1)
+	inputs = np.array(	[[0,0,1],
+						[1,1,1],
+						[1,0,1],
+						[0,1,1]])
+
+	keys = np.array([[0,1,1,0]]).T
+	weights = 2*np.random.random((3,1)) - 1
+	train(inputs, keys, weights)
+
+if __name__ == "__main__":
+	main()
